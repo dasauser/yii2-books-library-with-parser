7dc004fa5329d93590c95a03cc70f3a4b21503a8
dasauser <dasauser@gmail.com>
2024-08-08 16:41:31 +0300

    Создана таблица authors

---
+++ console/migrations/m240808_133520_create_authors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%authors}}`.
+ */
+class m240808_133520_create_authors_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%authors}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique(),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%authors}}');
+    }
+}
