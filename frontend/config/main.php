6f4c91c656115a4061438c25536268f91a947dc6
dasauser <dasauser@gmail.com>
2024-08-08 23:37:31 +0300

    Добавлены названия приложений

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name' => 'My Little Library',
     'defaultRoute' => 'books',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
