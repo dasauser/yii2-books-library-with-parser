ea2304d32c4f5e907392b322c8a46b72c72f405a
dasauser <dasauser@gmail.com>
2024-08-09 00:31:37 +0300

    Добавлен kartik-v/yii2-widget-select2

--- common/config/params.php
+++ common/config/params.php
@@ -6,4 +6,7 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
+
+    'bsVersion' => '5.x',
+    'bsDependencyEnabled' => false,
 ];
