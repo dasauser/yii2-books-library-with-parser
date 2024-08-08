a623f427900fb3564cc6af1be0e33ae2ec9f55ae
dasauser <dasauser@gmail.com>
2024-08-08 16:32:56 +0300

    Создана таблица books

---
+++ console/migrations/m240808_132502_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%books}}`.
+ */
+class m240808_132502_create_books_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%books}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255)->notNull()->unique(),
+            'isbn' => $this->string(20)->notNull()->unique(),
+            'pageCount' => $this->smallInteger(),
+            'publishedDate' => $this->dateTime(),
+            'thumbnailUrl' => $this->string(),
+            'thumbnailImage' => $this->string(),
+            'shortDescription' => $this->text(),
+            'longDescription' => $this->text(),
+            'status' => $this->string(),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%books}}');
+    }
+}
