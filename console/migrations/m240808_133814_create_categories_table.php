108adca2d36e24f98bb730982c9c3b8b95330690
dasauser <dasauser@gmail.com>
2024-08-08 16:45:36 +0300

    Создана таблица categories

---
+++ console/migrations/m240808_133814_create_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%categories}}`.
+ */
+class m240808_133814_create_categories_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%categories}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique(),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%categories}}');
+    }
+}
