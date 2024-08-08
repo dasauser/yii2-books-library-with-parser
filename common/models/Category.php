32633f8f79e2fd77457408da53d35b596d452e53
dasauser <dasauser@gmail.com>
2024-08-08 16:54:53 +0300

    Создана модель Category

---
+++ common/models/Category.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace common\models;
+
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "categories".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ *
+ * @property Book[] $books
+ */
+class Category extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'categories';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['name'], 'string', 'max' => 255],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::class,
+                'value' => new Expression('NOW()'),
+            ]
+        ];
+    }
+
+    /**
+     * Gets query for [[Books]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooks(): ActiveQuery
+    {
+        return $this->hasMany(Book::class, ['id' => 'book_id'])->viaTable('book_category', ['category_id' => 'id']);
+    }
+}
