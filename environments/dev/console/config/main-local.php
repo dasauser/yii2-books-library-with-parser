e5d0fb2419db51fbc15e16e29825f0cac1357ebb
dasauser <dasauser@gmail.com>
2024-08-08 15:45:54 +0300

    Изменен конфиг бд для console

--- environments/dev/console/config/main-local.php
+++ environments/dev/console/config/main-local.php
@@ -5,4 +5,13 @@ return [
     'modules' => [
         'gii' => 'yii\gii\Module',
     ],
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'mysql:host=localhost:3306;dbname=yii2advanced',
+            'username' => 'yii2advanced',
+            'password' => 'secret',
+            'charset' => 'utf8',
+        ],
+    ],
 ];
