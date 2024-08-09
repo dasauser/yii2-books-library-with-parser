1ce1971086d3eca9ce9bd50eb7a9a06aca8d1b15
dasauser <dasauser@gmail.com>
2024-08-09 15:58:17 +0300

    Добавлено halaxa/json-machine и yiisoft/yii2-queue

--- common/config/main.php
+++ common/config/main.php
@@ -2,12 +2,19 @@
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'bootstrap' => [
+        'queue',
+    ],
     'components' => [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
+        ],
+        'queue' => [
+            'class' => \yii\queue\file\Queue::class,
+            'as log' => \yii\queue\LogBehavior::class,
         ],
     ],
 ];
