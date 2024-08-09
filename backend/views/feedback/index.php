fc4dd60ead5b94c61fea3b3427628bfcadfefccc
dasauser <dasauser@gmail.com>
2024-08-09 08:41:18 +0300

    Добавлен просмотр и удаление обратной связи

---
+++ backend/views/feedback/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use backend\models\Feedback;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\FeedbackSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Feedbacks';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="feedback-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'email:email',
+            'name',
+            'body',
+            'phone',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Feedback $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 },
+                'template' => '{view}',
+            ],
+        ],
+    ]); ?>
+
+
+</div>
---
+++ backend/views/feedback/view.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/** @var yii\web\View $this */
+/** @var app\models\Feedback $model */
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Feedbacks', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="feedback-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'email:email',
+            'name',
+            'body',
+            'phone',
+        ],
+    ]) ?>
+
+</div>
